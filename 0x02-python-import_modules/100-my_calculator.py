4794b621bb98ac6291838b91cb146dcc87212d0b
adam-123406 <adamabdallahus@gmail.com>
2023-01-05 08:39:18 -0800

    my_calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,48 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    from calculator_1 import add, sub, mul, div
-    argv = sys.argv[1:]
-    argv_count = len(argv)
-    operators = ["+", "-", "*", "/"]
-    if argv_count is not 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    elif sys.argv[2] not in operators:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-    else:
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        if sys.argv[2] is "+":
-            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-        elif sys.argv[2] is "-":
-            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-        elif sys.argv[2] is "*":
-            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-        elif sys.argv[2] is "/":
-            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+def add(a, b):
+    """My addition function
+
+    Args:
+    a: first integer
+    b: second integer
+
+    Returns:
+    The return value. a + b
+    """
+    return (a + b)
+
+def sub(a, b):
+    """My subtraction function
+
+    Args:
+    a: first integer
+    b: second integer
+
+    Returns:
+    The return value. a - b
+    """
+    return (a - b)
+
+def mul(a, b):
+    """My multiplication function
+
+    Args:
+    a: first integer
+    b: second integer
+
+    Returns:
+    The return value. a * b
+    """
+    return (a * b)
+
+def div(a, b):
+    """My division function
+
+    Args:
+    a: first integer
+    b: second integer
+
+    Returns:
+    The return value. a / b
+    """
+    return int(a / b)
