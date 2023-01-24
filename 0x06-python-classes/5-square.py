2074007f45b7c3cfad377691c71e1093cdfde08d
adam-123406 <adamabdallahus@gmail.com>
2023-01-24 12:34:56 -0800

    task

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -45,7 +45,7 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         else:
-                self.__size = value
+            self.__size = value
 
     def my_print(self):
         """prints in stdout the square with the character #
