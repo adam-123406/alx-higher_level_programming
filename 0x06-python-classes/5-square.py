b66473b0ba6aad29111c32367cd89dea4e9cd180
adam-123406 <adamabdallahus@gmail.com>
2023-01-24 09:57:01 -0800

    task

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 """Defines a class Square"""
-
-
 class Square:
-    """
-    Class that defines properties of square by: (based on 4-square.py).
+    """Class that defines properties of square by: (based on 4-square.py).
     Attributes:
     size: size of a square (1 side).
     """
@@ -39,15 +36,15 @@ class Square:
             if not isinstance(value, int):
                 raise TypeError("size must be an integer")
             elif value < 0:
-            raise ValueError("size must be >= 0")
+                raise ValueError("size must be >= 0")
             else:
-            self.__size = value
+                self.__size = value
 
     def my_print(self):
         """prints in stdout the square with the character #
         """
 
-     if self.__size == 0:
-        print()
-    for i in range(self.__size):
-        print("#" * (self.__size))
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            print("#" * (self.__size))
