35ee66b5df011e8a67eee98fc476950587c11f63
adam-123406 <adamabdallahus@gmail.com>
2023-01-24 12:17:58 -0800

    task

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -3,8 +3,7 @@
 
 
 class Square:
-    """
-    Class that defines properties of square by: (based on 4-square.py).
+    """Class that defines properties of square by: (based on 4-square.py).
 
     Attribute
         size: size of a square (1 side).
