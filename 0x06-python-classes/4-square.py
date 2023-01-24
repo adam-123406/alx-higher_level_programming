d0a8854a08c75d206c34681bcf6efeb86ec4e55a
adam-123406 <adamabdallahus@gmail.com>
2023-01-24 09:44:55 -0800

    task

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,20 +5,20 @@ class Square:
         """Initialises the data"""
         self.size = size
 
-        def area(self):
-            """Returns current square area"""
-            return self.__size**2
+    def area(self):
+        """Returns current square area"""
+        return self.__size**2
 
-        @property
-        def size(self):
-            """Getter method"""
-            return self.__size
+    @property
+    def size(self):
+        """Getter method"""
+        return self.__size
 
-        @size.setter
-        def size(self, value):
-            """Setter method"""
-            self.__size = value
-            if type(value) != int:
-                raise TypeError("size must be an integer")
-            if value < 0:
-                raise ValueError("size must be >= 0")
+    @size.setter
+    def size(self, value):
+        """Setter method"""
+        self.__size = value
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
