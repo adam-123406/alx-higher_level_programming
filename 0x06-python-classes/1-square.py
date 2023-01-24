64d50f78794d33f107a81f0f87f733e69ef37cc5
adam-123406 <adamabdallahus@gmail.com>
2023-01-24 09:21:36 -0800

    task

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,8 +2,7 @@
 """Defines a class Square"""
 class Square:
     """Class that defines properties of square by: (based on 0-square.py).
-    Attributes:
-    size: size of a square (1 side)."""
+    Attributes:size: size of a square (1 side)."""
     def __init__(self, size):
         """Creates new instances of square (1 side).
         Args:
