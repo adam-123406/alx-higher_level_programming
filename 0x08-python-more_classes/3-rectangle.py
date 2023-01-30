0106ddd59150fda5ae3ab6de37e98103c2d70587
adam-123406 <adamabdallahus@gmail.com>
2023-01-30 01:18:54 -0800

    task3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -83,9 +83,9 @@ class Rectangle:
         (__height * 2).
         """
         if self.__width is 0 or self.__height is 0:
-            return 0
+        return 0
         else:
-            return (self.__width * 2) + (self.__height * 2)
+        return (self.__width * 2) + (self.__height * 2)
 
     def _draw_rectangle(self):
         """Formats a string of '#' and '\n' chars to print the rectangle
