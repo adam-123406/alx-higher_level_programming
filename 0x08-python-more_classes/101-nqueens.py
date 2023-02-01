b05b7a9435059ab2a05f74982942863a52337e2d
adam-123406 <adamabdallahus@gmail.com>
2023-02-01 14:59:04 -0800

    task101

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -120,7 +120,7 @@ if __name__ == "__main__":
         sys.exit(1)
     if sys.argv[1].isdigit() is False:
         print("N must be a number")
-    sys.exit(1)
+        sys.exit(1)
     if int(sys.argv[1]) < 4:
         print("N must be at least 4")
         sys.exit(1)
