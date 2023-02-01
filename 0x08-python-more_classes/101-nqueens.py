91497b99fa704f16769a8ac8005ae63baee51896
adam-123406 <adamabdallahus@gmail.com>
2023-02-01 15:13:25 -0800

    task101

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -110,8 +110,7 @@ def recursive_solve(board, row, queens, solutions):
             tmp_board = board_deepcopy(board)
             tmp_board[row][c] = "Q"
             xout(tmp_board, row, c)
-            solutions = recursive_solve(tmp_board, row + 1,
-    queens + 1, solutions)
+    solutions = recursive_solve(tmp_board, row + 1, queens + 1, solutions)
     return (solutions)
 
 
@@ -125,7 +124,6 @@ if __name__ == "__main__":
     if int(sys.argv[1]) < 4:
         print("N must be at least 4")
         sys.exit(1)
-
 
 board = init_board(int(sys.argv[1]))
 solutions = recursive_solve(board, 0, 0, [])
