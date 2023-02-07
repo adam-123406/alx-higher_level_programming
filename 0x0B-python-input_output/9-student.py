5909e6d625fd8ed5d6ced17c25d318cbc994e287
adam-123406 <adamabdallahus@gmail.com>
2023-02-07 02:05:50 -0800

    Student to JSON

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-"""method for student creation"""
+'''
+file: 11-student.py
+Classes:
+    -> Student
+'''
 
 
 class Student:
-    """Student obj"""
+    ''' Student class '''
 
     def __init__(self, first_name, last_name, age):
+        ''' Constructor method '''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        return self.__dict__
+        ''' Method that returns directory description '''
+        return self.__dict__.copy()
