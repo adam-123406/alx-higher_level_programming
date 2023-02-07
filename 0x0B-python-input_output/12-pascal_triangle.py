7864e737cd8e5ab8a85b0ec047e51c16c5ea755f
adam-123406 <adamabdallahus@gmail.com>
2023-02-07 02:49:47 -0800

    task

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -16,11 +16,11 @@ def pascal_triangle(n):
     triangle.append([1])
 
     for i in range(1, n):
-    before = triangle[-1]
-    after = [1]
-    for i in range(len(before) - 1):
-    after.append(before[i] + before[i + 1])
-    after += [1]
-    triangle.append(after)
+        before = triangle[-1
+        after = [1]
+        for i in range(len(before) - 1):
+            after.append(before[i] + before[i + 1])
+        after += [1]
+        triangle.append(after)
 
     return triangle
