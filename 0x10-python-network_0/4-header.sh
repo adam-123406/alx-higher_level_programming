56e98f1d635adf2e8d785d715f329feb40e14107
adam-123406 <adamabdallahus@gmail.com>
2023-04-28 23:13:50 +0400

    task

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a GET request with header variable being 'X-School-User-Id' with variable 98
+# send a GET request to the URL using curl, and display the body of the response
 curl -sH "X-School-User-Id: 98" "$1"
