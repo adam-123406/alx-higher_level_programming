d53a857054d12707b4965567b55a481e34d7de63
adam-123406 <adamabdallahus@gmail.com>
2023-04-28 22:32:05 +0400

    done

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 """
-Use requests package to make a get request to given URL and display
-the body of response, or error code if error.
+Use requests package to make a post request sending email param
+and display body of response.
 """
 import sys
 import requests
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    r = requests.get(url)
-    try:
-        r.raise_for_status()
-        print(r.text)
-    except Exception as e:
-        print("Error code: {}".format(r.status_code))
+    payload = {'email': sys.argv[2]}
+    r = requests.post(url, data=payload)
+    print(r.text)
