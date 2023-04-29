f89fb982e613a47ca60fcc223b648bd31338c608
adam-123406 <adamabdallahus@gmail.com>
2023-04-30 00:08:26 +0400

    done

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,24 +1,31 @@
 #!/usr/bin/python3
-"""This module contains a script that finds a peak in a list of unsorted
-integers"""
+""" Finds a peak in a list of unsorted integers
+"""
 
 
 def find_peak(list_of_integers):
-    """Finds a peak in a list of unsorted integers"""
-    length = len(list_of_integers)
-    if length == 0:
+    """
+    Args:
+        list_of_integers(int): list of integers to find peak of
+    Returns: peak of list_of_integers or None
+    """
+    size = len(list_of_integers)
+    mid_e = size
+    mid = size // 2
+
+    if size == 0:
         return None
-    peak = re_peak(list_of_integers, 0, length - 1)
-    return peak
 
-
-def re_peak(int_list, start, end):
-    if start == end:
-        return int_list[start]
-    mid = int((start + end) // 2)
-    if int_list[mid] > int_list[mid + 1] and int_list[mid] > int_list[mid - 1]:
-        return int_list[mid]
-    if int_list[mid + 1] > int_list[mid]:
-        return re_peak(int_list, mid + 1, end)
-    else:
-        return re_peak(int_list, 0, mid - 1)
+    while True:
+        mid_e = mid_e // 2
+        if (mid < size - 1 and
+                list_of_integers[mid] < list_of_integers[mid + 1]):
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid + mid_e // 2
+        elif mid_e > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid - mid_e // 2
+        else:
+            return list_of_integers[mid]
