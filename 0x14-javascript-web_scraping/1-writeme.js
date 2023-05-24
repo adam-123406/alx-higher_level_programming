bd39a5504c9b600d40989a42aa20b2cc87b6c8c2
adam-123406 <adamabdallahus@gmail.com>
2023-05-24 18:28:20 +0400

    done

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,3 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.writeFileSync(process.argv[2], process.argv[3]);
+const file = process.argv[2];
+const content = process.argv[3];
+fs.writeFile(file, content, 'utf-8', function (err) {
+  if (err) {
+    console.log(err);
+  }
+});
